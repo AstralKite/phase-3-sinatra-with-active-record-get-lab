bcfc07b857d269142f1a58db9e6d48eea12657ec
Fausto Ramirez <ramirezmedia@gmail.com>
2023-06-03 18:16:31 -0400

    commit tree 536aac7a42e940f11b11461b770952be4528d08d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+#make the json legible 
+set:default_content_type, 'application/json'
 
+  # add routes
+  get '/bakeries' do
+    Bakery.all.to_json
+  end
+
+  get '/bakeries/:id' do
+    id = params[:id]
+    Bakery.find(id).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.all.order(price: :desc).first.to_json
+  end
+  
 end
